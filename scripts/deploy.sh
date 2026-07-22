18de54dd59504868aa7cc0a047b9f25f9b8bb8c3
EmmanuelManalo <emmanuel.manalo@frontlinebusiness.com.ph>
2026-07-22 15:18:16 +0800

    add role/permission seeder in deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "$APP_ENV" = "production" ] || [ "$APP_ENV" = "staging" ] || [ "$APP_ENV" = "development" ]; then
     php artisan migrate --force
+    php artisan db:seed --class=RoleSeeder
 fi
 
 if [ "$APP_ENV" = "production" ]; then
