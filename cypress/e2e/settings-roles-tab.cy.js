5f56cf83c5e94e6f38e7bc577630061353dfa299
HerlynMae <torresherlynmae@gmail.com>
2026-07-30 13:47:55 +0800

    roles

--- cypress/e2e/settings-roles-tab.cy.js
+++ cypress/e2e/settings-roles-tab.cy.js
@@ -1,3 +1,5 @@
+const { Cylinder } = require('lucide-react');
+
 describe('Settings - Users - Roles Tab Page', () => {
     beforeEach(() => {
         cy.session(
@@ -93,46 +95,46 @@ describe('Settings - Users - Roles Tab Page', () => {
     //     cy.wait('@getRole').its('response.statusCode').should('eq', 200);
     // });
 
-    // //create
-    // it('should create a role', () => {
-    //     //esc key
-    //     cy.get('[data-cy="add-record-button"]').click();
-    //     cy.get('body').type('{esc}');
-    //     //close key
-    //     cy.get('[data-cy="add-record-button"]').click();
-    //     cy.get('[data-cy="modal-button-close-dialog"]', {
-    //         timeout: 1000,
-    //     }).click();
-    //     //cancel key
-    //     cy.get('[data-cy="add-record-button"]').click();
-    //     cy.get('[data-cy="button-button-1"]', {
-    //         timeout: 1000,
-    //     })
-    //         .eq(0)
-    //         .click();
+    //create
+    it('should create a role', () => {
+        //esc key
+        cy.get('[data-cy="add-record-button"]').click();
+        cy.get('body').type('{esc}');
+        //close key
+        cy.get('[data-cy="add-record-button"]').click();
+        cy.get('[data-cy="modal-button-close-dialog"]', {
+            timeout: 1000,
+        }).click();
+        //cancel key
+        cy.get('[data-cy="add-record-button"]').click();
+        cy.get('[data-cy="button-button-1"]', {
+            timeout: 1000,
+        })
+            .eq(0)
+            .click();
 
-    //     //create
-    //     cy.get('[data-cy="add-record-button"]').click();
-    //     cy.get('[data-cy="role-modal-input-e-g-program-coordinator"]').type(
-    //         'Manager',
-    //     );
-    //     cy.get('[data-cy="role-modal-button-button"]').eq(0).click();
-    //     cy.get('[data-cy="role-modal-input-checkbox"]').eq(5).click();
-    //     cy.get('[data-cy="role-modal-input-checkbox"]').eq(8).click();
-    //     cy.get('[data-cy="role-modal-button-button"]').eq(0).click();
+        //create
+        cy.get('[data-cy="add-record-button"]').click();
+        cy.get('[data-cy="role-modal-input-e-g-program-coordinator"]').type(
+            'Manager',
+        );
+        cy.get('[data-cy="role-modal-button-button"]').eq(0).click();
+        cy.get('[data-cy="role-modal-input-checkbox"]').eq(5).click();
+        cy.get('[data-cy="role-modal-input-checkbox"]').eq(8).click();
+        cy.get('[data-cy="role-modal-button-button"]').eq(0).click();
 
-    //     cy.intercept('POST', '**/roles').as('createRole');
-    //     cy.get('[data-cy="button-button-1"]').eq(1).click();
+        cy.intercept('POST', '**/roles').as('createRole');
+        cy.get('[data-cy="button-button-1"]').eq(1).click();
 
-    //     cy.wait('@createRole').then((interception) => {
-    //         console.log(interception.response);
-    //     });
+        cy.wait('@createRole').then((interception) => {
+            console.log(interception.response);
+        });
 
-    //     cy.contains('Manager', {
-    //         timeout: 1000,
-    //     }).should('be.visible');
-    //     cy.get('[data-cy="toast-div-3"]').should('be.visible');
-    // });
+        cy.contains('Manager', {
+            timeout: 1000,
+        }).should('be.visible');
+        cy.get('[data-cy="toast-div-3"]').should('be.visible');
+    });
 
     //update
     it('should update a role', () => {
@@ -148,6 +150,82 @@ describe('Settings - Users - Roles Tab Page', () => {
             .eq(0)
             .should('be.visible')
             .click();
+
         cy.get('body').type('{esc}');
+
+        //close
+        cy.contains('[data-cy="settings-row-div-4"]', 'Manager')
+            .should('be.visible')
+            .find('[data-cy="row-menu-button-row-actions"]')
+            .click();
+
+        cy.get('[data-cy="row-menu-button-4"]')
+            .eq(0)
+            .should('be.visible')
+            .click();
+        cy.get('[data-cy="modal-button-close-dialog"]').click();
+
+        //cancel
+        cy.contains('[data-cy="settings-row-div-4"]', 'Manager')
+            .should('be.visible')
+            .find('[data-cy="row-menu-button-row-actions"]')
+            .click();
+
+        cy.get('[data-cy="row-menu-button-4"]')
+            .eq(0)
+            .should('be.visible')
+            .click();
+
+        cy.get('[data-cy="button-button-1"]').eq(0).click();
+
+        //update
+        cy.contains('[data-cy="settings-row-div-4"]', 'Manager')
+            .should('be.visible')
+            .find('[data-cy="row-menu-button-row-actions"]')
+            .click();
+
+        cy.get('[data-cy="row-menu-button-4"]')
+            .eq(0)
+            .should('be.visible')
+            .click();
+
+        cy.get('[data-cy="role-modal-input-e-g-program-coordinator"]').clear().type('Managers').should('have.value', 'Managers');
+
+        cy.get('[data-cy="role-modal-button-button"]').eq(0).click();
+
+        cy.get('[data-cy="button-button-1"]').eq(1).click();
+    });
+
+    //delete
+    it('should delete a role', () => {
+        cy.intercept('DELETE', '**/settings-roles/**').as('deleteRole');
+
+        //cancel
+        cy.contains('[data-cy="settings-row-div-4"]', 'Manager')
+            .should('be.visible')
+            .find('[data-cy="row-menu-button-row-actions"]')
+            .click();
+
+        cy.get('[data-cy="row-menu-button-4"]')
+            .eq(1)
+            .should('be.visible')
+            .click();
+
+        cy.get('[data-cy="confirm-delete-modal-button-button"]').click();
+
+        //delete
+        cy.contains('[data-cy="settings-row-div-4"]', 'Manager')
+            .should('be.visible')
+            .find('[data-cy="row-menu-button-row-actions"]')
+            .click();
+
+        cy.get('[data-cy="row-menu-button-4"]')
+            .eq(1)
+            .should('be.visible')
+            .click();
+
+        cy.get('[data-cy="confirm-delete-modal-button-button-2"]').click();
+
+        cy.get('[data-cy="toast-div-3"]').should('be.visible');
     });
 });
